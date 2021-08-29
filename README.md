404ba43444b61cf6dc4d0136d69c986ade3c26a5
? <alberto.garcia.2016@upb.edu.co>
2021-08-28 21:00:53 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# py2Tex
+ This is a project for "translating" stuff to Tex.
+
+## How to use it?
+By now, it only accepts Keras models and returns summary.
+Call it by py2Tex.m2Tex(model,modelName), it should return a Tex formula for your model summary.
+
+Credits to https://stackoverflow.com/users/16504277/nico
+
+Feel free to contribute.
---
+++ py2Tex.py
@@ -0,0 +1,35 @@
+r"""
+==================================================
+Little script for "translating" stuff to Tex
+
+Implemented by intentodemusico and nico
+OP: https://stackoverflow.com/users/16504277/nico
+
+==================================================
+TODO documentation
+
+"""
+__author__ = 'intentodemusico'
+
+
+def m2tex(model,modelName):
+    stringlist = []
+    model.summary(line_length=70, print_fn=lambda x: stringlist.append(x))
+    del stringlist[1:-4:2]
+    del stringlist[-1]
+    for ix in range(1,len(stringlist)-3):
+        tmp = stringlist[ix]
+        stringlist[ix] = tmp[0:31]+"& "+tmp[31:59]+"& "+tmp[59:]+"\\\\ \hline"
+    stringlist[0] = "Model: {} \\\\ \hline".format(modelName)
+    stringlist[1] = stringlist[1]+" \hline"
+    stringlist[-4] += " \hline"
+    stringlist[-3] += " \\\\"
+    stringlist[-2] += " \\\\"
+    stringlist[-1] += " \\\\ \hline"
+    prefix = ["\\begin{table}[]", "\\begin{tabular}{lll}"]
+    suffix = ["\end{tabular}", "\caption{{Model summary for {}.}}".format(modelName), "\label{tab:model-summary}" , "\end{table}"]
+    stringlist = prefix + stringlist + suffix 
+    out_str = " \n".join(stringlist)
+    out_str = out_str.replace("_", "\_")
+    out_str = out_str.replace("#", "\#")
+    print(out_str)
